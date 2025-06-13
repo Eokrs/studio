babb4249ca882d4f8d37035ead754c7bb0f4a962
Caio Oliveira <caioitap27@gmail.com>
2025-06-13 00:41:39 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HeroSection>
    
    wit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { HeroSection } from '@/components/sections/HeroSection';
+// import { HeroSection } from '@/components/sections/HeroSection';
 import { ProductShowcaseSection } from '@/components/sections/ProductShowcaseSection';
 import { AboutSection } from '@/components/sections/AboutSection';
 // import { GallerySection } from '@/components/sections/GallerySection';
@@ -9,7 +9,7 @@ import { MotionLazyContainer } from '@/components/animations/MotionLazyContainer
 export default function HomePage() {
   return (
     <MotionLazyContainer>
-      <HeroSection />
+      {/* <HeroSection /> */}
       <ProductShowcaseSection />
       <AboutSection />
       {/* <GallerySection /> */}
